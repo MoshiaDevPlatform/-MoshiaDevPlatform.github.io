5c25f3df4e6854af0a7c59275b6c2aa15e5968df
MoshiaDevPlatform <51088735+MoshiaDevPlatform@users.noreply.github.com>
2021-02-20 04:14:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var router = require('router')
+
+var app = express;
+
+app.use('/', function(req, res){
+     res.redirect(index.html)
+});
+
+app.Router('/admin', function(req, res){
+    res.
+})
+
+
+
+
+
+var port = 3000;
+const server = htt.createServer(app);
+server.listen(port);
+
